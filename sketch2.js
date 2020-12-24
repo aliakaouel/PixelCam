478de843b3ccb1c733031c82dc8b948981bce406
aliakaouel <31700227+aliakaouel@users.noreply.github.com>
2020-12-24 12:51:47 +0100

    new methode

--- sketch2.js
+++ sketch2.js
@@ -1,12 +1,12 @@
 
 //background
-let sketch0 = function(p){
+let sketch0 = function(p0){
 
   let mySky;
 
-  p.preload = function(){
+  p0.preload = function(){
     // mySky = p0.loadImage("Sky.jpg");
-    thisH1 = p.createElement("h1", 'Just you, <br>  yourself <br> and pixels');
+    thisH1 = p0.createElement("h1", 'Just you, <br>  yourself <br> and pixels');
     thisH1.position(50, 0);
     thisH1.style('font-family', 'mittwoch, serif')
     thisH1.style('font-size', '100px')
@@ -14,14 +14,14 @@ let sketch0 = function(p){
 
   }
 
-  p.setup = function(){
-    let initialCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
+  p0.setup = function(){
+    let initialCanvas = p0.createCanvas(p.windowWidth, p.windowHeight);
 
 
   }
 
-  p.draw = function(){
-    p.background("LemonChiffon");
+  p0.draw = function(){
+    p0.background("LemonChiffon");
     // p0.image(mySky, 0, 0, p0.width, p0.height);
 
   }
@@ -108,28 +108,28 @@ let sketch1 = function(p){
 
 
 //Frame 2
-let sketch2 = function(p){
+let sketch2 = function(p2){
   let frame2;
   let x2 = 640;
   let y2 = 480;
 
-  p.preload = function(){
-    frame2 = p.loadImage("frame-02-05.png");
+  p2.preload = function(){
+    frame2 = p2.loadImage("frame-02-05.png");
   }
 
-  p.setup = function(){
-    let secondCanvas = p.createCanvas(300, 250);
+  p2.setup = function(){
+    let secondCanvas = p2.createCanvas(300, 250);
     secondCanvas.position(500, 100);
-    capture2 = p.createCapture(p2.VIDEO);
+    capture2 = p2.createCapture(p2.VIDEO);
     capture2.hide();
 
   }
 
-  p.draw = function(){
-    p.image(capture2,x2, y2);
-    p.background('LemonChiffon');
-    p.noStroke();
-    p.fill(0);
+  p2.draw = function(){
+    p2.image(capture2,x2, y2);
+    p2.background('LemonChiffon');
+    p2.noStroke();
+    p2.fill(0);
     capture2.loadPixels();
     let pixelSize = 5;
 
@@ -139,60 +139,60 @@ let sketch2 = function(p){
          let i = y * x2 + x;
          let darkness = (255 - capture2.pixels[i * 4]) / 255;
          let d = pixelSize * darkness;
-         p.rect(x, y, d, d);
+         p2.rect(x, y, d, d);
 
 
       }
     }
 
 
-    p.image(frame2, 0, 0, 300, 250);
+    p2.image(frame2, 0, 0, 300, 250);
   }
 }
 
 
 //Frame 3
-// let sketch3 = function(p3){
-//   let frame1;
-//   let x3 = 640;
-//   let y3= 480;
-//
-//   p3.preload = function(){
-//     frame1 = p3.loadImage("frame-03.png");
-//   }
-//
-//   p3.setup = function(){
-//     let thirdCanvas = p3.createCanvas(500, 400);
-//     thirdCanvas.position(750, 150);
-//     capture3 = p3.createCapture(p3.VIDEO);
-//     capture3.hide();
-//   }
-//
-//   p3.draw = function(){
-//     // p3.background('LemonChiffon');
-//    p3.noStroke();
-//    p3.fill("Tomato");
-//     p3.image(capture3,0, 0, x3, y3);
-//
-//     p3.background("LemonChiffon");
-//     capture3.loadPixels();
-//     const circleSize = 5;
-//     for (let y = 0; y < y3; y += circleSize) {
-//      for (let x = 0; x < x3; x += circleSize) {
-//      const i = y * x3 + x;
-//      const darkness = (255 - capture3.pixels[i * 4]) / 255;
-//      const radius = circleSize * darkness;
-//      p3.ellipse(x, y, radius, radius);
-//    }
-//
-//     //Frame
-//     p3.image(frame1, 0, 0, 500, 400);
-//   }
-// }
-// }
+let sketch3 = function(p3){
+  let frame1;
+  let x3 = 640;
+  let y3= 480;
+
+  p3.preload = function(){
+    frame1 = p3.loadImage("frame-03.png");
+  }
+
+  p3.setup = function(){
+    let thirdCanvas = p3.createCanvas(500, 400);
+    thirdCanvas.position(750, 150);
+    capture3 = p3.createCapture(p3.VIDEO);
+    capture3.hide();
+  }
+
+  p3.draw = function(){
+    // p3.background('LemonChiffon');
+   p3.noStroke();
+   p3.fill("Tomato");
+    p3.image(capture3,0, 0, x3, y3);
+
+    p3.background("LemonChiffon");
+    capture3.loadPixels();
+    const circleSize = 5;
+    for (let y = 0; y < y3; y += circleSize) {
+     for (let x = 0; x < x3; x += circleSize) {
+     const i = y * x3 + x;
+     const darkness = (255 - capture3.pixels[i * 4]) / 255;
+     const radius = circleSize * darkness;
+     p3.ellipse(x, y, radius, radius);
+   }
+
+    //Frame
+    p3.image(frame1, 0, 0, 500, 400);
+  }
+}
+}
 
 
 let p0 = new p5(sketch0);
-let p1 = new p5(sketch1);
+let p = new p5(sketch1);
 let p2 = new p5(sketch2);
 let p3 = new p5(sketch3)
